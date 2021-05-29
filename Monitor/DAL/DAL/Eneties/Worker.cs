fae81db479e64bf5d4ceadc1710ca638837420c5
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-05-29 20:16:24 +0300

    Create Worker.cs

---
+++ Monitor/DAL/DAL/Eneties/Worker.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Enities
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual Test Test { get; set; }
+
+
+
+    }
+}
