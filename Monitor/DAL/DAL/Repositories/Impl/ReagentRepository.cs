58ef1df1ac29cea437306e4cd7aa79c9c096a7f0
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-05-29 20:16:19 +0300

    Create ReagentRepository.cs

---
+++ Monitor/DAL/DAL/Repositories/Impl/ReagentRepository.cs
@@ -0,0 +1,20 @@
+﻿using DAL.EF;
+using DAL.Enities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class ReagentRepository : BaseRepository<Reagent>, IReagentRepository
+    {
+        internal ReagentRepository(GeoEntContext context)
+         : base(context)
+        {
+
+        }
+    }
+}
