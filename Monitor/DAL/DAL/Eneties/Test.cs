23d20d32fa2873b3362140a5db0d87f14ae1ed3b
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-05-29 20:16:27 +0300

    Create Test.cs

---
+++ Monitor/DAL/DAL/Eneties/Test.cs
@@ -0,0 +1,20 @@
+﻿using DAL.EF;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Enities
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Res { get; set; }
+        public virtual Reagent Reagent { get; set; }
+        public virtual Worker Worker { get; set; }
+
+    }
+}
