283817fd658d945fa60a92174fd156bb2193736f
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-05-31 21:21:42 +0300

    Create TestWorkerRepository.cs

---
+++ Monitor/DAL.Test/DAL.Test/TestWorkerRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Enities;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Test
+{
+    class TestWorkerRepository : BaseRepository<Worker>
+    {
+        public TestWorkerRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
