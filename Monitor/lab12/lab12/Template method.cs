f35a22aece3e178501153c338fb5e1e9095fb79b
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-01 18:51:04 +0300

    Create Template method.cs

---
+++ Monitor/lab12/lab12/Template method.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scientist scientist = new Scientist();
+            Assistent assistent = new Assistent();
+
+            scientist.Testing();
+            assistent.Testing();
+
+            Console.Read();
+        }
+
+        abstract class Test
+        {
+            public void Testing()
+            {
+                TakeSample();
+                ProcessSample();
+                Investigate();
+                WriteNotes();
+            }
+            public virtual void TakeSample()
+            {
+                Console.WriteLine("Берем образец");
+            }
+            public virtual void ProcessSample()
+            {
+                Console.WriteLine("Обрабатываем образец");
+
+            }
+            public abstract void Investigate();
+            public virtual void WriteNotes()
+            {
+                Console.WriteLine("Документируем");
+
+            }
+
+
+        }
+
+        class Scientist : Test
+        {
+            public override void Investigate()
+            {
+                Console.WriteLine("Исследуем образец с помощью спец приборов доступны только на должности ученого");
+            }
+        }
+        class Assistent : Test
+        {
+            public override void Investigate()
+            {
+                Console.WriteLine("Исследуем образец с помощью спец приборов доступны только на должности ассистента");
+            }
+        }
+    }
+}
+
