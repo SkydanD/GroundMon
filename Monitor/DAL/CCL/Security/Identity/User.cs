37e788038963e4d2987eac5f144c68147ca7658b
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-01 15:01:27 +0300

    Create User.cs

---
+++ Monitor/DAL/CCL/Security/Identity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public abstract class User
+    {
+        public User(int userId, string name, int workerId, string userType)
+        {
+            UserId = userId;
+            Name = name;
+            WorkerId = workerId;
+            UserType = userType;
+        }
+        public int UserId { get; }
+        public string Name { get; }
+        public int WorkerId { get; }
+        public string UserType { get; set; }
+    }
+}
