afe0e0941e992a2bdb29b347883324443bc41d01
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-01 18:51:14 +0300

    Create Decorator .cs

---
+++ Monitor/lab11/lab11/Decorator .cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reagent reag = new Wheland();
+            reag = new WhelandIntermediate(reag); // Wheland Intermidiate
+            Console.WriteLine("Название: {0}", reag.Name);
+            Console.WriteLine("Количество: {0}", reag.GetQty());
+            Console.ReadLine();
+        }
+
+
+        abstract class Reagent
+        {
+            public Reagent(string n)
+            {
+                this.Name = n;
+            }
+            public string Name { get; protected set; }
+            public abstract int GetQty();
+        }
+
+        class Wheland : Reagent
+        {
+            public Wheland() : base("Wheland")
+            { }
+            public override int GetQty()
+            {
+                return 4;
+            }
+        }
+
+        abstract class ReagentDecorator : Reagent
+        {
+            protected Reagent reagent;
+            public ReagentDecorator(string n, Reagent reagent) : base(n)
+            {
+                this.reagent = reagent;
+            }
+        }
+
+        class WhelandIntermediate : ReagentDecorator
+        {
+            public WhelandIntermediate(Reagent r)
+            : base(r.Name + " Intermidiate", r)
+            { }
+
+            public override int GetQty()
+            {
+                return reagent.GetQty() + 13;
+            }
+        }
+
+
+    }
+}
+
