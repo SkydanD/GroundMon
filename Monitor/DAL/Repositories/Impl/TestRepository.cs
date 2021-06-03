7a66901935ca1949da0c8a5b89d833b90961f212
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-03 18:25:20 +0300

    Create TestRepository.cs

---
+++ Monitor/DAL/Repositories/Impl/TestRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.EF;
+using DAL.Enities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class TestRepository : BaseRepository<Test>, ITestRepository
+    {
+        internal TestRepository(GeoEntContext context) : base(context)
+        {
+        }
+    }
+}
