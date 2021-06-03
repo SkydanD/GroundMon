d1c7f592aa7b9c83e7521a52dc34030ec082d6e7
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-03 18:25:17 +0300

    Create WorkerRepository.cs

---
+++ Monitor/DAL/Repositories/Impl/WorkerRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.EF;
+using DAL.Enities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class WorkerRepository : BaseRepository<Worker>, IWorkerRepository
+    {
+        internal WorkerRepository(GeoEntContext context) : base(context)
+        {
+        }
+    }
+}
