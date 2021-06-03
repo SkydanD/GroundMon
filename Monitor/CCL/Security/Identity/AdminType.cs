3bf4b4a9f146cb2012d01fae511258798252b6f0
SkydanD <84770746+SkydanD@users.noreply.github.com>
2021-06-03 18:27:14 +0300

    Create AdminType.cs

---
+++ Monitor/CCL/Security/Identity/AdminType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public class AdminType : User
+    {
+        public AdminType(int userId, string name, int workerId)
+            : base(userId, name, workerId, nameof(AdminType))
+        {
+
+        }
+    }
+}
